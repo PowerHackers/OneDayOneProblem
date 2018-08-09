76b510d0e0040883c8f6a4c250149da20e45c9fa
PowerHackers <crazyjerryh@gmail.com>
2018-08-10 01:13:45 +0800

    Date: 8-10 Description: amend

--- poj3984.java
+++ poj3984.java
@@ -1,65 +1,168 @@
 package org.powerhackers;
 
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class App
 {
-   public static void main(String []args) {
-      Scanner scanner = new Scanner(System.in);
-      int n = scanner.nextInt();
-      scanner.nextLine();
-      String stra = scanner.nextLine();
-      String strb = scanner.nextLine();
+    public static void main( String[] args )
+    {
+        Solution solution = new Solution();
+        solution.read();
+        solution.solve(0);
+        solution.print();
+    }
 
-      char lista[] = strb.toCharArray();
-      char listb[] = stra.toCharArray();
+    public static class Point {
+        int x;
+        int y;
 
-      List<Integer> res = new ArrayList<Integer>();
-      int size = 0;
-      for(int i = 0; i < lista.length; i++){
-         int j = listb.length - 1;
-         //System.out.println(lista[i]);
-         boolean flag = false;
-         for(; j >=i; j--) {
-             if(lista[i] == listb[j]) {
-                 flag = true;
-                 break;
-             }
-         }
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
 
-         if(!flag) {
-             size = -1;
-             break;
-         } else {
-             for(;j > i; j--) {
-                 res.add(j);
-                 char a = listb[j];
-                 listb[j] = listb[j-1];
-                 listb[j-1] = a;
-             }
-         }
-      }
+        @Override
+        public String toString() {
+            return "(" + x + ", " + y + ")";
+        }
+    }
 
-      if(size != -1) {
-          size = res.size();
-      }
-      System.out.println(size);
-      StringBuilder sb = new StringBuilder();
-      if(size > 0) {
-         sb.append(res.get(0));
-      }
+    public static class Solution {
+        private static final int MAXN = 5;
+        private static final int INF = 0xffff;
+        private int map[][] = new int[MAXN][MAXN];
+        private int vis[][] = new int[MAXN][MAXN];
+        private int dis[][] = new int[MAXN][MAXN];
+        private Point prepath[][] = new Point[MAXN][MAXN];
+        private Point postpath[][] = new Point[MAXN][MAXN];
+        private int flag = 0;
 
-      if(size > 1) {
-         for(int i = 1; i < size; i++) {
-             sb.append(" ").append(res.get(i));
-         }
-      }
+        private int dir[][] = {
+                {0,1}, {1,0}, {-1,0},{0,-1}
+        };
 
-      if(size > 0) {
-          System.out.println(sb.toString());
-      }
-   }
+        public Solution() {
+            init();
+        }
+
+        public void init() {
+            for(int i = 0; i < MAXN; i++) {
+                for(int j = 0; j < MAXN; j++) {
+                    dis[i][j] = INF;
+                }
+            }
+        }
+
+        public void read() {
+            Scanner scanner = new Scanner(System.in);
+            for(int i = 0; i < MAXN; i++) {
+                for(int j = 0; j < MAXN; j++){
+                    map[i][j] = scanner.nextInt();
+                }
+                //scanner.nextLine();
+            }
+        }
+
+        public void solve(int flag) {
+            this.flag = flag;
+            if(flag == 0) {
+                bfs();
+            } else {
+                dfs(0,0);
+            }
+        }
+
+        private void bfs() {
+            Queue<Point> queue = new LinkedList<Point>();
+            queue.offer(new Point(0,0));
+
+            vis[0][0] = 1;
+            prepath[0][0] = null;
+            while(!queue.isEmpty()) {
+                Point point = queue.poll();
+                int x = point.x;
+                int y = point.y;
+
+                for(int i = 0; i < 4; i++) {
+                    int xx = x + dir[i][0];
+                    int yy = y + dir[i][1];
+                    if(xx >= 0 && yy >= 0 && xx < MAXN && yy < MAXN && vis[xx][yy] == 0 && map[xx][yy] != 1) {
+                        Point np = new Point(xx, yy);
+                        prepath[xx][yy] = point;
+                        vis[xx][yy] = 1;
+                        queue.offer(np);
+                    }
+                }
+            }
+        }
+
+        private void dfs(int x, int y) {
+            if(x < 0 || y < 0 || x >= MAXN || y >= MAXN) {
+                return;
+            }
+
+            if(x == MAXN -1 && y == MAXN -1) {
+                dis[x][y] = 0;
+                postpath[x][y] = null;
+                return;
+            }
+
+            if(vis[x][y] == 1 || map[x][y] == 1) {
+                return;
+            }
+
+            vis[x][y] = 1;
+            for(int i = 0; i < 4; i++) {
+               int xx =  x + dir[i][0];
+               int yy =  y + dir[i][1];
+
+               if(xx < 0 || yy < 0 || xx >= MAXN || yy >= MAXN) {
+                   continue;
+               }
+
+               dfs(xx, yy);
+               if(dis[xx][yy] + 1 < dis[x][y]) {
+                   dis[x][y] = dis[xx][yy] +1;
+                   postpath[x][y] = new Point(xx, yy);
+               }
+            }
+        }
+
+        private void printbfs() {
+            Point point = new Point(MAXN - 1, MAXN -1);
+            List<Point> list = new ArrayList<Point>();
+            StringBuilder sb = new StringBuilder();
+            while(point != null) {
+                list.add(point);
+                int x = point.x;
+                int y = point.y;
+                point = prepath[x][y];
+            }
+
+            for(int i = list.size() - 1; i >= 0; i--) {
+                sb.append(list.get(i)).append('\n');
+            }
+            System.out.print(sb.toString());
+        }
+
+        private void printdfs() {
+            Point point = new Point(0, 0);
+            StringBuffer sb = new StringBuffer();
+            while(point != null) {
+                sb.append(point).append('\n');
+                int x = point.x;
+                int y = point.y;
+                point = postpath[x][y];
+            }
+            System.out.print(sb.toString());
+        }
+
+        public void print() {
+            if(flag == 0) {
+                printbfs();
+            } else {
+                printdfs();
+            }
+        }
+    }
 }
